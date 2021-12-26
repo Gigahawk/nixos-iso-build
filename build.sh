efd7906c78137798548a90120fb28a9d01a03407
Jasper Chan <jasperchan515@gmail.com>
2021-12-26 04:19:22 -0800

    fix build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nixos-generate --format-path ./install-iso-gnome.nix --configuration configuration.sh
+nixos-generate --format-path ./install-iso-gnome.nix --configuration configuration.nix
