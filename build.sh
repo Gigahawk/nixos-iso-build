ff084ef53a18006d488dece6dc1b92b073046ce3
Jasper Chan <jasperchan515@gmail.com>
2022-01-03 23:17:53 -0800

    partial support for building multiple iso types

--- build.sh
+++ build.sh
@@ -2,11 +2,17 @@
 
 exec 5>&1
 
+outpath="dist/$1"
+
+rm -rf dist
+mkdir dist
 
 echo "Starting build"
 
 out=$(nixos-generate --format-path ./install-iso-gnome.nix --configuration configuration.nix | tee >(cat - >&5))
 
-echo "Build finished"
+echo "Build finished, iso written to $out"
 
-echo "$out"
+echo "Copying to $outpath"
+cp "$1" "$outpath"
+
