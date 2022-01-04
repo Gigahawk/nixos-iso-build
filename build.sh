778c4a12953a347d897aeb2696c46657b2747f05
Jasper Chan <jasperchan515@gmail.com>
2022-01-03 17:54:33 -0800

    Attempt to store nixos-generate log to variable

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-nixos-generate --format-path ./install-iso-gnome.nix --configuration configuration.nix
+exec 5>&1
+
+
+echo "Starting build"
+
+out=$(nixos-generate --format-path ./install-iso-gnome.nix --configuration configuration.nix | tee >(cat - >&5))
+
+echo "Build finished"
+
+echo "$out"
