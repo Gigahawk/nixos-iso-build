f1eb79888ec7266cd2fd615adfbe24fa7418c9c6
Jasper Chan <jasperchan515@gmail.com>
2022-01-04 02:51:19 -0800

    Fix typo

--- build.sh
+++ build.sh
@@ -2,17 +2,17 @@
 
 exec 5>&1
 
-outpath="dist/$1"
+out_path="dist/$1"
 
 rm -rf dist
 mkdir dist
 
 echo "Starting build"
 
-out=$(nixos-generate --format-path ./install-iso-gnome.nix --configuration configuration.nix | tee >(cat - >&5))
+iso_path=$(nixos-generate --format-path ./install-iso-gnome.nix --configuration configuration.nix | tee >(cat - >&5))
 
-echo "Build finished, iso written to $out"
+echo "Build finished, iso written to $iso_path"
 
-echo "Copying to $outpath"
-cp "$1" "$outpath"
+echo "Copying to $out_path"
+cp "$iso_path" "$out_path"
 
